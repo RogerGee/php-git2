4a06ce1201561a0e39f065ae51060529f8dc4968
Roger Gee <rpg11a@acu.edu>
2017-02-24 23:26:14 -0600

    Add new and improve existing odb_backend methods

--- php-odb-backend.cpp
+++ php-odb-backend.cpp
@@ -12,16 +12,14 @@ using namespace php_git2;
 
 // Class method entries
 
-// string GitODBBackend::read(&$size,&$type,$oid);
+// string GitODBBackend::read(&$type,$oid);
 static PHP_METHOD(GitODBBackend,read);
-ZEND_BEGIN_ARG_INFO_EX(GitODBBackend_read_arginfo,0,0,3)
-    ZEND_ARG_PASS_INFO(1)
+ZEND_BEGIN_ARG_INFO_EX(GitODBBackend_read_arginfo,0,0,2)
     ZEND_ARG_PASS_INFO(1)
 ZEND_END_ARG_INFO()
 
 static PHP_METHOD(GitODBBackend,read_prefix);
-ZEND_BEGIN_ARG_INFO_EX(GitODBBackend_read_prefix_arginfo,0,0,4)
-    ZEND_ARG_PASS_INFO(1)
+ZEND_BEGIN_ARG_INFO_EX(GitODBBackend_read_prefix_arginfo,0,0,3)
     ZEND_ARG_PASS_INFO(1)
     ZEND_ARG_PASS_INFO(1)
 ZEND_END_ARG_INFO()
@@ -36,7 +34,12 @@ static PHP_METHOD(GitODBBackend,write);
 static PHP_METHOD(GitODBBackend,writestream);
 static PHP_METHOD(GitODBBackend,readstream);
 static PHP_METHOD(GitODBBackend,exists);
+
 static PHP_METHOD(GitODBBackend,exists_prefix);
+ZEND_BEGIN_ARG_INFO_EX(GitODBBackend_exists_prefix_arginfo,0,0,2)
+    ZEND_ARG_PASS_INFO(1)
+ZEND_END_ARG_INFO()
+
 static PHP_METHOD(GitODBBackend,refresh);
 static PHP_METHOD(GitODBBackend,foreach);
 static PHP_METHOD(GitODBBackend,writepack);
@@ -49,7 +52,7 @@ zend_function_entry php_git2::odb_backend_methods[] = {
     PHP_ME(GitODBBackend,writestream,NULL,ZEND_ACC_PUBLIC)
     PHP_ME(GitODBBackend,readstream,NULL,ZEND_ACC_PUBLIC)
     PHP_ME(GitODBBackend,exists,NULL,ZEND_ACC_PUBLIC)
-    PHP_ME(GitODBBackend,exists_prefix,NULL,ZEND_ACC_PUBLIC)
+    PHP_ME(GitODBBackend,exists_prefix,GitODBBackend_exists_prefix_arginfo,ZEND_ACC_PUBLIC)
     PHP_ME(GitODBBackend,refresh,NULL,ZEND_ACC_PUBLIC)
     PHP_ME(GitODBBackend,foreach,NULL,ZEND_ACC_PUBLIC)
     PHP_ME(GitODBBackend,writepack,NULL,ZEND_ACC_PUBLIC)
@@ -200,115 +203,102 @@ static void convert_oid_fromstr(git_oid* dest,const char* src,int srclen)
 
 PHP_METHOD(GitODBBackend,read)
 {
-    zval* zsize;
     zval* ztype;
     char* strOid;
     int strOidLen;
     php_odb_backend_object* object;
+    void* data = NULL;
+    size_t size = 0;
+    git_otype type = GIT_OBJ_ANY;
+    git_oid oid;
+
+    // Grab object backing. Verify that the backend exists and the function is
+    // implemented.
+    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+    if (object->backend == nullptr || object->backend->read_prefix == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::read(): function is not available");
+        return;
+    }
 
     // Grab parameters. The first two parameters are passed by reference so we
     // have to extract their zvals.
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"zzs",&zsize,&ztype,&strOid,
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"zs",&ztype,&strOid,
             &strOidLen) == FAILURE)
     {
         return;
     }
 
-    // Grab object backing.
-    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
-
-    // If the odb_backend exists, then we can actually call the underlying
-    // function. The backend only exists if we retrieved the object from an
-    // existing ODB or if we passed the custom backend to an ODB setter which
-    // prompted it to be created.
-    if (object->backend != nullptr) {
-        void* data = NULL;
-        size_t size = 0;
-        git_otype type = GIT_OBJ_ANY;
-        git_oid oid;
-
-        // Interpret the string parameter as a human-readable OID. Convert it
-        // and then call read().
-        try {
-            convert_oid_fromstr(&oid,strOid,strOidLen);
-            if (object->backend->read(&data,&size,&type,object->backend,&oid) < 0) {
-                git_error();
-            }
-        } catch (php_git2_exception ex) {
-            zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
-            return;
+    // Interpret the string parameter as a human-readable OID. Convert it
+    // and then call read().
+    try {
+        convert_oid_fromstr(&oid,strOid,strOidLen);
+        if (object->backend->read(&data,&size,&type,object->backend,&oid) < 0) {
+            git_error();
         }
-
-        // Copy the result into the return zval. Then set the out
-        // parameters. Finally we have to free the buffer allocated by the call
-        // to read().
-        RETVAL_STRINGL((const char*)data,size,1);
-        ZVAL_LONG(zsize,size);
-        ZVAL_LONG(ztype,type);
-        free(data);
+    } catch (php_git2_exception ex) {
+        zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
         return;
     }
 
-    RETURN_FALSE;
+    // Copy the result into the return zval. Then set the out
+    // parameters. Finally we have to free the buffer allocated by the call
+    // to read().
+    RETVAL_STRINGL((const char*)data,size,1);
+    ZVAL_LONG(ztype,type);
+    free(data);
 }
 
 PHP_METHOD(GitODBBackend,read_prefix)
 {
     zval* zoid;
-    zval* zsize;
     zval* ztype;
     char* strOid;
     int strOidLen;
     php_odb_backend_object* object;
 
+    // Grab object backing. Verify that the backend exists and the function is
+    // implemented.
+    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+    if (object->backend == nullptr || object->backend->read_prefix == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::read_prefix(): function is not available");
+        return;
+    }
+
     // Grab parameters. The first three parameters are passed by reference so we
     // have to extract their zvals.
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"zzzs",&zoid,&zsize,&ztype,
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"zzs",&zoid,&ztype,
             &strOid,&strOidLen) == FAILURE)
     {
         return;
     }
 
-    // Grab object backing.
-    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+    void* data = NULL;
+    size_t size = 0;
+    git_otype type = GIT_OBJ_ANY;
+    git_oid prefix;
+    git_oid full;
+    char buf[GIT_OID_HEXSZ + 1];
 
-    // If the odb_backend exists, then we can actually call the underlying
-    // function. The backend only exists if we retrieved the object from an
-    // existing ODB or if we passed the custom backend to an ODB setter which
-    // prompted it to be created.
-    if (object->backend != nullptr) {
-        void* data = NULL;
-        size_t size = 0;
-        git_otype type = GIT_OBJ_ANY;
-        git_oid prefix;
-        git_oid full;
-        char buf[GIT_OID_HEXSZ + 1];
-
-        // Interpret the string parameter as a human-readable OID. Convert it
-        // and then call read_prefix().
-        try {
-            convert_oid_fromstr(&prefix,strOid,strOidLen);
-            if (object->backend->read_prefix(&full,&data,&size,&type,object->backend,&prefix,strOidLen) < 0) {
-                git_error();
-            }
-        } catch (php_git2_exception ex) {
-            zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
-            return;
+    // Interpret the string parameter as a human-readable OID. Convert it and
+    // then call read_prefix().
+    try {
+        convert_oid_fromstr(&prefix,strOid,strOidLen);
+        if (object->backend->read_prefix(&full,&data,&size,&type,object->backend,&prefix,strOidLen) < 0) {
+            git_error();
         }
-
-        // Copy the result into the return zval. Then set the out
-        // parameters. Finally we have to free the buffer allocated by the call
-        // to read().
-        RETVAL_STRINGL((const char*)data,size,1);
-        git_oid_tostr(buf,sizeof(buf),&full);
-        ZVAL_STRING(zoid,buf,1);
-        ZVAL_LONG(zsize,size);
-        ZVAL_LONG(ztype,type);
-        free(data);
+    } catch (php_git2_exception ex) {
+        zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
         return;
     }
 
-    RETURN_FALSE;
+    // Copy the result into the return zval. Then set the out
+    // parameters. Finally we have to free the buffer allocated by the call to
+    // read().
+    RETVAL_STRINGL((const char*)data,size,1);
+    git_oid_tostr(buf,sizeof(buf),&full);
+    ZVAL_STRING(zoid,buf,1);
+    ZVAL_LONG(ztype,type);
+    free(data);
 }
 
 PHP_METHOD(GitODBBackend,read_header)
@@ -318,6 +308,17 @@ PHP_METHOD(GitODBBackend,read_header)
     char* strOid;
     int strOidLen;
     php_odb_backend_object* object;
+    size_t size = 0;
+    git_otype type = GIT_OBJ_ANY;
+    git_oid oid;
+
+    // Grab object backing. Verify that the backend exists and the function is
+    // implemented.
+    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+    if (object->backend == nullptr || object->backend->read_header == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::read_header(): function is not available");
+        return;
+    }
 
     // Grab parameters. The first two parameters are passed by reference so we
     // have to extract their zvals.
@@ -327,42 +328,61 @@ PHP_METHOD(GitODBBackend,read_header)
         return;
     }
 
-    // Grab object backing.
-    object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
-
-    // If the odb_backend exists, then we can actually call the underlying
-    // function. The backend only exists if we retrieved the object from an
-    // existing ODB or if we passed the custom backend to an ODB setter which
-    // prompted it to be created.
-    if (object->backend != nullptr) {
-        size_t size = 0;
-        git_otype type = GIT_OBJ_ANY;
-        git_oid oid;
-
-        // Interpret the string parameter as a human-readable OID. Convert it
-        // and then call read().
-        try {
-            convert_oid_fromstr(&oid,strOid,strOidLen);
-            if (object->backend->read_header(&size,&type,object->backend,&oid) < 0) {
-                git_error();
-            }
-        } catch (php_git2_exception ex) {
-            zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
-            return;
+    // Interpret the string parameter as a human-readable OID. Convert it
+    // and then call read().
+    try {
+        convert_oid_fromstr(&oid,strOid,strOidLen);
+        if (object->backend->read_header(&size,&type,object->backend,&oid) < 0) {
+            git_error();
         }
-
-        // This varient just returns the header as output parameters and returns
-        // true.
-        ZVAL_LONG(zsize,size);
-        ZVAL_LONG(ztype,type);
-        RETURN_TRUE;
+    } catch (php_git2_exception ex) {
+        zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
+        return;
     }
 
-    RETURN_FALSE;
+    // This varient just returns the header as output parameters and returns
+    // true.
+    ZVAL_LONG(zsize,size);
+    ZVAL_LONG(ztype,type);
+
+    RETURN_TRUE;
 }
 
 PHP_METHOD(GitODBBackend,write)
 {
+    char* uoid;
+    int uoidSize;
+    char* data;
+    int dataSize;
+    long type;
+    git_oid oid;
+    php_odb_backend_object* object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+
+    // Backend must be created and write function must be available.
+    if (object->backend == nullptr || object->backend->write == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::write(): function is not available");
+        return;
+    }
+
+    // Grab parameters.
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"ssl",&uoid,&uoidSize,
+            &data,&dataSize,&type) == FAILURE)
+    {
+        return;
+    }
+
+    // Convert OID hex string to oid structure and call underlying function.
+    try {
+        convert_oid_fromstr(&oid,uoid,uoidSize);
+        if (object->backend->write(object->backend,&oid,data,dataSize,(git_otype)type) < 0) {
+            git_error();
+        }
+    } catch (php_git2_exception ex) {
+        zend_throw_exception(nullptr,ex.what(),0 TSRMLS_CC);
+        return;
+    }
+
+    RETURN_TRUE;
 }
 
 PHP_METHOD(GitODBBackend,writestream)
@@ -375,10 +395,68 @@ PHP_METHOD(GitODBBackend,readstream)
 
 PHP_METHOD(GitODBBackend,exists)
 {
+    int retval;
+    char* uoid;
+    int uoidSize;
+    git_oid oid;
+    php_odb_backend_object* object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+
+    // Backend must be created.
+    if (object->backend == nullptr || object->backend->exists == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::exists(): function is not available");
+        return;
+    }
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"s",&uoid,&uoidSize) == FAILURE) {
+        return;
+    }
+
+    // Convert OID hex string to oid structure and call underlying function.
+    convert_oid_fromstr(&oid,uoid,uoidSize);
+    retval = object->backend->exists(object->backend,&oid);
+    if (retval) {
+        RETURN_TRUE;
+    }
+    RETURN_FALSE;
 }
 
 PHP_METHOD(GitODBBackend,exists_prefix)
 {
+    int retval;
+    zval* zoid;
+    char* uoid;
+    int uoidSize;
+    git_oid oid;
+    git_oid fullOid;
+    php_odb_backend_object* object = LOOKUP_OBJECT(php_odb_backend_object,getThis());
+
+    // Backend must be created.
+    if (object->backend == nullptr || object->backend->exists_prefix == nullptr) {
+        php_error(E_ERROR,"GitODBBackend::exists_prefix(): function is not available");
+        return;
+    }
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"zs",&zoid,&uoid,&uoidSize) == FAILURE) {
+        return;
+    }
+
+    // Convert OID hex string to oid structure and call underlying function.
+    convert_oid_fromstr(&oid,uoid,uoidSize);
+    retval = object->backend->exists_prefix(&fullOid,object->backend,&oid,uoidSize);
+
+    if (retval) {
+        char buf[GIT_OID_HEXSZ + 1];
+
+        // Set the out variable to the full OID string.
+        git_oid_tostr(buf,sizeof(buf),&fullOid);
+        ZVAL_STRING(zoid,buf,1);
+
+        RETURN_TRUE;
+    }
+
+    // No need to set the out variable. PHP already did this if it was a new
+    // variable. Otherwise it will just keep its old value.
+    RETURN_FALSE;
 }
 
 PHP_METHOD(GitODBBackend,refresh)
