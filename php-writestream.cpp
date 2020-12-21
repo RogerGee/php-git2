d79ae1521028e35d10f7ded2a0ca43b017090781
Roger Gee <rpg11a@acu.edu>
2020-12-21 14:34:28 -0600

    Update php-writestream object functionality

--- php-writestream.cpp
+++ php-writestream.cpp
@@ -18,26 +18,29 @@ zend_function_entry php_git2::writestream_methods[] = {
 
 // Make function implementation
 
-void php_git2::php_git2_make_writestream(zval* zp,git_writestream* ws TSRMLS_DC)
+void php_git2::php_git2_make_writestream(zval* zp,git_writestream* ws)
 {
     php_writestream_object* obj;
-    zend_class_entry* ce = php_git2::class_entry[php_git2_writestream_obj];
 
     // Create object zval.
-    object_init_ex(zp,ce);
-    obj = reinterpret_cast<php_writestream_object*>(zend_objects_get_address(zp TSRMLS_CC));
-
+    object_init_ex(zp,php_git2::class_entry[php_git2_writestream_obj]);
+    obj = php_zend_object<php_writestream_object>::get_storage(zp);
     obj->ws = ws;
+}
+
+// php_zend_object init function
+
+template<>
+void php_zend_object<php_writestream_object>::init(zend_class_entry* ce)
+{
+    handlers.get_constructor = php_git2::not_allowed_get_constructor;
 }
 
 // Implementation of php_writestream_object
 
-/*static*/ zend_object_handlers php_writestream_object::handlers;
-php_writestream_object::php_writestream_object(zend_class_entry* ce TSRMLS_DC):
-    ws(nullptr), zts(TSRMLS_C)
+php_writestream_object::php_writestream_object():
+    ws(nullptr)
 {
-    zend_object_std_init(this,ce TSRMLS_CC);
-    object_properties_init(this,ce);
 }
 
 php_writestream_object::~php_writestream_object()
@@ -45,33 +48,28 @@ php_writestream_object::~php_writestream_object()
     if (ws != nullptr) {
         ws->free(ws);
     }
-
-    zend_object_std_dtor(this ZTS_MEMBER_CC(zts));
-}
-
-/*static*/ void php_writestream_object::init(zend_class_entry* ce)
-{
-    handlers.get_constructor = php_git2::not_allowed_get_constructor;
 }
 
 // Userspace method implementations
 
 PHP_METHOD(GitWritestream,write)
 {
-    php_bailer bailer ZTS_CTOR;
+    php_bailer bailer;
     int retval;
     char* buffer;
-    int bufsz;
-    php_writestream_object* object = LOOKUP_OBJECT(php_writestream_object,getThis());
+    size_t bufsz;
+    php_writestream_object* object;
+
+    object = php_zend_object<php_writestream_object>::get_storage(getThis());
 
     // The writestream must exist and the function must be implemented.
     if (object->ws == nullptr || object->ws->write == nullptr) {
-        php_error(E_ERROR,"GitWritestream::write(): method is not available");
+        zend_throw_error(nullptr,"GitWritestream::write(): method is not available");
         return;
     }
 
     // Parse parameters.
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"s",&buffer,&bufsz) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS(),"s",&buffer,&bufsz) == FAILURE) {
         return;
     }
 
@@ -81,31 +79,34 @@ PHP_METHOD(GitWritestream,write)
         if (retval < 0) {
             php_git2::git_error(retval);
         }
+
     } catch (php_git2::php_git2_exception_base& ex) {
-        php_bailout_context ctx(bailer TSRMLS_CC);
+        php_bailout_context ctx(bailer);
 
         if (BAILOUT_ENTER_REGION(ctx)) {
-            ex.handle(TSRMLS_C);
+            ex.handle();
         }
     }
 }
 
 PHP_METHOD(GitWritestream,close)
 {
-    php_bailer bailer ZTS_CTOR;
+    php_bailer bailer;
     int retval;
     char* buffer;
-    int bufsz;
-    php_writestream_object* object = LOOKUP_OBJECT(php_writestream_object,getThis());
+    size_t bufsz;
+    php_writestream_object* object;
+
+    object = php_zend_object<php_writestream_object>::get_storage(getThis());
 
     // The writestream must exist and the function must be implemented.
     if (object->ws == nullptr || object->ws->close == nullptr) {
-        php_error(E_ERROR,"GitWritestream::close(): method is not available");
+        zend_throw_error(nullptr,"GitWritestream::close(): method is not available");
         return;
     }
 
     // Parse parameters.
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"s",&buffer,&bufsz) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS(),"s",&buffer,&bufsz) == FAILURE) {
         return;
     }
 
@@ -115,11 +116,12 @@ PHP_METHOD(GitWritestream,close)
         if (retval < 0) {
             php_git2::git_error(retval);
         }
+
     } catch (php_git2::php_git2_exception_base& ex) {
-        php_bailout_context ctx(bailer TSRMLS_CC);
+        php_bailout_context ctx(bailer);
 
         if (BAILOUT_ENTER_REGION(ctx)) {
-            ex.handle(TSRMLS_C);
+            ex.handle();
         }
     }
 }
