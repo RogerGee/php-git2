5d3051f50e34839d22c04185cb01b951e60de1d6
Roger Gee <rpg11a@acu.edu>
2023-05-19 11:37:38 -0500

    Add arginfo for GitWritestream class

---
+++ stubs/GitWritestream_arginfo.h
@@ -0,0 +1,9 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 6ab89ec59965b74fb5ddceca48c7fbe3b8c4d403 */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_GitWritestream_write, 0, 1, IS_VOID, 0)
+	ZEND_ARG_TYPE_INFO(0, buffer, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_GitWritestream_close, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
